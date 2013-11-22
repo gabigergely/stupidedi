edfd3fd6e29722738230256aafd5b53c7df6ccbe
Gabriella Gergely <gabi@purchasingplatform.com>
2013-11-21 18:33:32 -0600

    Add GA to PO850 guide

--- lib/stupidedi/contrib/004010/guides/PO850.rb
+++ lib/stupidedi/contrib/004010/guides/PO850.rb
@@ -74,7 +74,7 @@ module Stupidedi
                 r::Required, d::RepeatCount.bounded(1),
                 b::Element(e::Situational, "Assigned Identification"),
                 b::Element(e::Situational, "Quantity Ordered"),
-                b::Element(e::Situational, "Unit or Basis for Measurement Code", b::Values("CA", "EA", "LB")),
+                b::Element(e::Situational, "Unit or Basis for Measurement Code", b::Values("CA", "EA", "LB", "GA")),
                 b::Element(e::Situational, "Unit Price"),
                 b::Element(e::Situational, "Basis of Unit Price Code", b::values("PE","PP","UM")),
                 b::Element(e::Situational, "Product/Service ID Qualifier", b::Values("VN", "VC")),
